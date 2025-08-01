ad052666c3bae3b34842bdea93bf5b95e191de70
franc <f.m.covarrubias96@gmail.com>
2025-07-31 20:41:06 -0400

    Se han descomentado el codigo de la tarea_01

--- tareas/Soluciones/solucion_tarea_01.sql
+++ tareas/Soluciones/solucion_tarea_01.sql
@@ -4,25 +4,25 @@
 SELECT * FROM users;
 
 -- 2. Ver el registro cuyo id sea igual a 10
--- SELECT *
--- FROM users 
--- WHERE id = 10;
+SELECT *
+FROM users 
+WHERE id = 10;
 
 -- 3. Quiero todos los registros que cuyo primer nombre sea Jim (engañosa)
--- SELECT *
--- FROM users
--- WHERE name LIKE 'Jim %';
+SELECT *
+FROM users
+WHERE name LIKE 'Jim %';
 
 -- 4. Todos los registros cuyo segundo nombre es Alexander
--- SELECT *
--- FROM users
--- WHERE name LIKE '% Alexander';
+SELECT *
+FROM users
+WHERE name LIKE '% Alexander';
 
 -- 5. Cambiar el nombre del registro con id = 1, por tu nombre Ej:'Fernando Herrera'
--- 	UPDATE users
--- 	SET
--- 		name = 'Franco Covarrubias'
--- 	WHERE id = 1;
+	UPDATE users
+	SET
+		name = 'Franco Covarrubias'
+	WHERE id = 1;
 
 -- 6. Borrar el último registro de la tabla
 SELECT MAX(id)
